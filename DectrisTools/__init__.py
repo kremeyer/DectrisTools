1fff7d736c2d82f5a667356426b1543175d3252d
Laurenz Kremeyer <laurenz.kremeyer@mail.mcgill.ca>
2022-08-17 18:19:48 -0400

    added imports for top-level access

--- DectrisTools/__init__.py
+++ DectrisTools/__init__.py
@@ -1,4 +1,6 @@
 import os
+# imports for top-level access
+from lib.processing import SingleShotDataset, SingleShotProcessor
 
 VERSION = "0.1"
 IP = "fe80::4ed9:8fff:feca:a8f9"
@@ -10,3 +12,4 @@ def get_base_path():
     returns package base dir
     """
     return os.path.dirname(__file__)
+
