30841c1e9d96ab8bcf3593927005a8044eda1608
Laurenz Kremeyer <laurenz.kremeyer@mail.mcgill.ca>
2022-12-08 18:26:56 -0500

    tests, masks as roi

--- DectrisTools/tests/utils.py
+++ DectrisTools/tests/utils.py
@@ -1,17 +1,16 @@
 """
 variables required for testing functionality of the package
 """
+from os.path import join
 import numpy as np
-
+from DectrisTools import get_base_path
 
 STACKSIZE = 1_000
 IMG_SHAPE = (512, 512)
 
-
 IMAGES = np.random.randint(0, 100, (STACKSIZE, IMG_SHAPE[0], IMG_SHAPE[1]), dtype=np.uint16)
 MASK = np.random.randint(0, 1, IMG_SHAPE, dtype=np.uint16)
 NORM_VALUES = np.random.random(STACKSIZE).astype(np.float32)
-
 
 SLICES = [
     slice(0, 100, 2),
@@ -28,7 +27,6 @@ SLICES = [
     slice(-10, -100, None),
 ]
 
-
 TUPLES = [
     (0, 100, 2),
     (10, 100, -2),
@@ -39,7 +37,9 @@ TUPLES = [
     (-10, 100, 2),
     (-10, -100, -2),
     (-10, -100, 2),
-    (NaN, -100, 2),
-    (-10, NaN, 2),
-    (-10, -100, NaN),
+    (np.NaN, -100, 2),
+    (-10, np.NaN, 2),
+    (-10, -100, np.NaN),
 ]
+
+LOGFILE = join(get_base_path(), 'tests', 'experiment.log')
